eca5407df2254d2a82f4e512ea51cebfd7fb5970
Quentin TREHEUX <qtreheux@localhost.localdomain>
2020-02-17 10:01:38 +0100

    [FIX] display_h.c

--- src/display_h.c
+++ src/display_h.c
@@ -10,10 +10,10 @@
 void display_h(void)
 {
     printf("USAGE\n");
-    printf("\t%s", USE);
-    printf("\nDESCRITION\n");
-    printf("   n\t%s", N);
-    printf("   k\t%s", K);
-    printf("   i0\t%s", I0);
-    printf("   i1\t%s", I1);
+    printf("    %s", USE);
+    printf("\nDESCRIPTION\n");
+    printf("    n\t   %s", N);
+    printf("    k\t   %s", K);
+    printf("    i0\t   %s", I0);
+    printf("    i1\t   %s", I1);
 }
