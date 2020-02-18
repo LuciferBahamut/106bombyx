49550cc754d90084706f8129d68612a973aa60ac
Quentin TREHEUX <qtreheux@localhost.localdomain>
2020-02-18 13:45:07 +0100

    [ADD] bombyx growth rate

--- src/bombyx.c
+++ src/bombyx.c
@@ -7,6 +7,26 @@
 
 #include "bombyx.h"
 
+void growth_rate(char **av)
+{
+    float n = atof(av[1]);
+    float k = atof(av[2]);
+    float xi = n;
+    float xn;
+    int i = 1;
+
+    printf("%d %2.f\n", i, n);
+    for (int i = 2; i != 101; i++) {
+        xn = k * xi * ((1000 - xi) / 1000);
+        xi = xn;
+        printf("%d %.2f\n", i, xn);
+    }
+}
+
 void bombyx(int ac, char **av)
 {
+    if (ac == 3)
+        growth_rate(av);
+    // else
+    //    ;
 }
