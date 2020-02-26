c4e8c9c96a43b2d529486487712f75fd511a4c0b
Quentin TREHEUX <qtreheux@localhost.localdomain>
2020-02-26 15:13:08 +0100

    [ADD] round

--- src/bombyx.c
+++ src/bombyx.c
@@ -7,24 +7,33 @@
 
 #include "bombyx.h"
 
-void generations(char **av)
+double round_it(double xn)
+{
+    xn *= 100;
+    xn = round(xn);
+    xn /= 100;
+    return (xn);
+}
+
+void generations(char **av, int u)
 {
     double n = atof(av[1]);
     int i0 = atoi(av[2]);
     int i1 = atoi(av[3]);
     double xi = 0;
     double xn = 0;
-    int u = 0;
 
     for (double k = 1.00; u != 4; k += 0.01, u = (int) k) {
         xi = n;
         for (int i = 1; i != i0 + 1; i++) {
             xn = k * xi * ((1000 - xi) / 1000);
             xi = xn;
+            xn = round_it(xn);
         }
         for (int z = i0; z != i1 + 1; z++) {
             xn = k * xi * ((1000 - xi) / 1000);
             xi = xn;
+            xn = round_it(xn);
             printf("%.2f %.2f\n", k, xn);
         }
     }
@@ -41,6 +50,7 @@ void growth_rate(char **av)
     for (int i = 2; i != 101; i++) {
         xn = k * xi * ((1000 - xi) / 1000);
         xi = xn;
+        xn = round_it(xn);
         printf("%d %.2f\n", i, xn);
     }
 }
@@ -50,5 +60,5 @@ void bombyx(int ac, char **av)
     if (ac == 3)
         growth_rate(av);
     else
-        generations(av);
+        generations(av, 0);
 }
